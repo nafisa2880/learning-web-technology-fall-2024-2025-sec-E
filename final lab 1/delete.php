4f7db6c154fd94248d02e1186347e6156859aaf9
nafisa2880 <nafisamoon548@gmail.com>
2024-12-23 13:24:27 +0600

    Add files via upload

---
+++ final lab 1/delete.php
@@ -0,0 +1,21 @@
+<?php
+include 'db.php';
+session_start();
+if (!isset($_COOKIE['status'])) {
+    header('location: login.html');  
+    exit;
+}
+ 
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+ 
+    foreach ($_SESSION['users'] as $key => $user) {
+        if ($user['id'] == $id) {
+            unset($_SESSION['users'][$key]);
+            break;
+        }
+    }
+ 
+    header('location: userlist.php');
+}
+?>
